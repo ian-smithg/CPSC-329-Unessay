a6fc95ce95a8cb7fa7592dd52d94cf5118865cd5
ian-smithg <125311683+ian-smithg@users.noreply.github.com>
2023-04-11 17:58:01 -0600

    updates

--- index.js
+++ index.js
@@ -4,20 +4,13 @@ let code2 = 'bat';
 let code3 = 'mat';
 let code4 = 'tat';
 
+var one = false;
+var two = false;
+var three = false;
 
 
 function setUp(){
-    var one = localStorage.getItem('in1');
-    var two = localStorage.getItem('in2');
-    var three = localStorage.getItem('in3');
-    var four = localStorage.getItem('in4');
     
-    if(one !== 'true' && two !== 'true' && three !== 'true' && four !== 'true'){
-        localStorage.setItem('in1','false');
-        localStorage.setItem('in2','false');
-        localStorage.setItem('in3','false');
-        localStorage.setItem('in4','false');
-    }
 
     if(one == 'true'){
         updateElement('input1','btn1','txt1');
@@ -43,12 +36,12 @@ function test(){
 
 function return1(){
    
-    var one = localStorage.getItem('in1');
    // if(one === 'false'){
         var input = document.getElementById('input1').value;
         if(input == code1){
             updateElement('input1','btn1','txt1');
-            //localStorage.setItem('in1', 'true');
+            one = true;
+            checkComplete();
         }
    // }
 
@@ -57,11 +50,12 @@ function return1(){
 
 function return2(){
    
-    var two = localStorage.getItem('in2');
    // if(two === 'false'){
         var input = document.getElementById('input2').value;
         if(input == code2){
             updateElement('input2','btn2','txt2');
+            two = true;
+            checkComplete();
         }
    // }
 
@@ -69,29 +63,16 @@ function return2(){
 
 function return3(){
 
-    var three = localStorage.getItem('in3');
    // if(three === 'false'){
         var input = document.getElementById('input3').value;
         if(input == code3){
             updateElement('input3','btn3','txt3');
+            three = true;
+            checkComplete();
         }
    // }
 
 }
-
-function return4(){
-    var four = localStorage.getItem('in4');
-   // if(four === 'false'){
-        var input = document.getElementById('input4').value;
-        if(input == code4){
-            updateElement('input4','btn4','txt4');
-        }
-   // }
-
-}
-
-
-
 
 
 
@@ -102,6 +83,12 @@ function updateElement(form, button, text){
     document.getElementById(button).style.border = '3px solid #ff0000'
     document.getElementById(button).style.color = '#ff0000'
     document.getElementById(text).style.color ='#ff0000'
+}
 
-
+function checkComplete(){
+    console.log('called');
+    if(one === true && two === true && three === true){
+        window.location.replace("final.html");
+        console.log('working');
+    }
 }
